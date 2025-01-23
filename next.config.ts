f5bd8ce58e8c150a9c07c1ec450c014dccd76d0e
yc97463 <hi@imych.one>
2025-01-24 02:57:47 +0800

    feat: update Next.js configuration to remove basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/',
-  assetPrefix: '/',
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
