e1cbdcfafa141304e2bebf13930f03e213cbf2af
yc97463 <hi@imych.one>
2025-01-24 17:46:34 +0800

    feat: configure Next.js to disable image optimization for unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // 不使用最佳化圖片
+  },
   basePath: '',
   assetPrefix: '',
 };
